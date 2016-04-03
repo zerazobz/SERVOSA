3e30e470df9a0e199da7ce02e7fcca41495e872b
Erick Asto Oblitas <zerazobz@outlook.com>
2016-04-03 18:28:20 -0500

    ->Web Application

---
+++ SERVOSA.SAIR.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SERVOSA.SAIR.WEB.MvcApplication" Language="C#" %>
---
+++ SERVOSA.SAIR.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SERVOSA.SAIR.WEB.Startup))]
+namespace SERVOSA.SAIR.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
