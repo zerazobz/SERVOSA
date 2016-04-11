f36ee313a42565df74a041bdb0dc0cacbba4e7ac
Erick Asto Oblitas <zerazobz@outlook.com>
2016-04-11 08:20:33 -0500

    ->Procedures Updated

--- DATABASE/Stored Procedures/dbo.SAIR_VEHIS.sql
+++ DATABASE/Stored Procedures/dbo.SAIR_VEHIS.sql
@@ -1,23 +1,12 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
 CREATE PROCEDURE [dbo].[SAIR_VEHIS]
-	@prmStart int,
-	@prmEnd int
 AS
-	DECLARE @totalRows int
 BEGIN
-	SELECT @totalRows = count(*) from SAIR_VEHICLE;
-
-	select DATA.* from
-	(
-		select ROW_NUMBER() OVER(ORDER BY VEHI.Codigo) RowNumber, @totalRows AS TotalRows, VEHI.Item, VEHI.Codigo, VEHI.PlacaTracto, VEHI.PlacaTolva, VEHI.CodigoMarca, VEHI.CodigoEstado
-		from SAIR_VEHICLE VEHI
-	) DATA
-	where DATA.RowNUmber >= @prmStart and DATA.RowNUmber <= @prmEnd;
+	select VEHI.Item, VEHI.Codigo, VEHI.PlacaTracto, VEHI.PlacaTolva, VEHI.CodigoMarca, VEHI.CodigoEstado
+	from SAIR_VEHICLE VEHI;
 END
-
-
 GO
---
+++ DATABASE/Stored Procedures/dbo.SAIR_VEHIS_Filtrado.sql
@@ -0,0 +1,20 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[SAIR_VEHIS_Filtrado]
+	@prmStart int,
+	@prmEnd int
+AS
+	DECLARE @totalRows int
+BEGIN
+	SELECT @totalRows = count(*) from SAIR_VEHICLE;
+
+	select DATA.* from
+	(
+		select ROW_NUMBER() OVER(ORDER BY VEHI.Codigo) RowNumber, @totalRows AS TotalRows, VEHI.Item, VEHI.Codigo, VEHI.PlacaTracto, VEHI.PlacaTolva, VEHI.CodigoMarca, VEHI.CodigoEstado
+		from SAIR_VEHICLE VEHI
+	) DATA
+	where DATA.RowNUmber >= @prmStart and DATA.RowNUmber <= @prmEnd;
+END
+GO
