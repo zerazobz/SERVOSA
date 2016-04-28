8d302fbbe6840e1e4a79d42584eee115dbb617fc
Erick Asto Oblitas <zerazobz@outlook.com>
2016-04-28 08:06:15 -0500

    ->improvements

---
+++ SERVOSA.SAIR.WEB/Controllers/VehicleDataController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SERVOSA.SAIR.WEB.Controllers
+{
+    public class VehicleDataController : Controller
+    {
+        public ActionResult Data()
+        {
+            return View();
+        }
+    }
+}
