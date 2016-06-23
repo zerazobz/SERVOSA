9db38ea4c431f0be85ad8fc0b806bb823c157c81
Erick Asto Oblitas <erickastooblitas@gmail.com>
2016-06-23 08:16:02 -0500

    ->Styles little changes

--- SERVOSA.SAIR.WEB/App_Start/BundleConfig.cs
+++ SERVOSA.SAIR.WEB/App_Start/BundleConfig.cs
@@ -30,18 +30,22 @@ namespace SERVOSA.SAIR.WEB
                       "~/Scripts/jquery.isotope.min.js",
                       "~/Scripts/jquery.prettyPhoto.js",
                       "~/Scripts/main.js",
-                      "~/Scripts/wow.min.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/wow.min.js"
+                      , "~/Scripts/respond.js"
+                      //, "~/Scripts/quirk.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                        //"~/Content/bootstrap.css",
-                        "~/Content/animate.min.css",
-                        "~/Content/bootstrap-corlate.min.css",
-                        "~/Content/font-awesome.min.css",
-                        "~/Content/main.css",
-                        "~/Content/prettyPhoto.css",
-                        "~/Content/responsive.css",
-                      "~/Content/site.css"));
+                      //"~/Content/bootstrap.css",
+                      "~/Content/animate.min.css",
+                      "~/Content/bootstrap-corlate.min.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/main.css",
+                      "~/Content/prettyPhoto.css",
+                      "~/Content/responsive.css"
+                      , "~/Content/site.css"
+                      //, "~/Content/quirk.css"
+                      ));
         }
     }
 }
